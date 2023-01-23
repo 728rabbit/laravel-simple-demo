f6e5952703850861660bb99a5aea7e3a09ed8b31
728rabbit <kaiyunchan2012@hotmail.com>
2023-01-23 17:34:13 +0800

    Add files via upload

---
+++ lang/zh-hk/web.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'welcome' => '歡迎使用Larave 9演示系統'
+];
---
+++ resources/views/web/home.blade.php
@@ -0,0 +1,7 @@
+@extends('web.main')
+
+@section('content')
+<h1>{{ __('web.welcome') }}</h1>
+@endsection
+                
+       
